6916dc13d6c782b2bd470c3d5853a415a7c2f12f
jcoester <12155189+jcoester@users.noreply.github.com>
2024-07-10 09:45:07 +0200

    Update LaunchApp.java

--- bfme-resfix/src/main/java/launch/LaunchApp.java
+++ bfme-resfix/src/main/java/launch/LaunchApp.java
@@ -3,11 +3,10 @@ package launch;
 import javax.swing.*;
 import gui.MainFrame;
 
-import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
-
-import static gui.MainFrame.logger;
 
 public class LaunchApp {
 
@@ -17,27 +16,43 @@ public class LaunchApp {
         launchApp();
     }
 
+    /**
+     * Initializes the LOCALAPPDATA directory used for Logback logging.
+     */
     private static void initLocalLogPath() {
         // Get the LOCALAPPDATA environment variable and construct the log directory path
-        String appDataLocal = System.getenv("LOCALAPPDATA") + "\\bfme-resfix";
+        String appDataLocal = System.getenv("LOCALAPPDATA");
+        if (appDataLocal != null) {
+            Path logDirPath = Paths.get(appDataLocal, "bfme-resfix");
 
-        // Ensure the directory exists
-        if (Files.notExists(Paths.get(appDataLocal)))
-            if (!new File(appDataLocal).mkdirs())
-                System.err.println("Error when creating LOCALAPPDATA/bfme-resfix directory");
-
-        // Set the system property for Logback
-        System.setProperty("appdata.local", appDataLocal);
+            // Ensure the directory exists
+            try {
+                if (Files.notExists(logDirPath)) {
+                    Files.createDirectory(logDirPath);
+                }
+                System.setProperty("appdata.local", logDirPath.toString());
+            } catch (Exception e) {
+                System.err.println("Error in initLocalLogPath(): " + e);
+            }
+        } else {
+            System.err.println("LOCALAPPDATA environment variable is not set.");
+        }
     }
 
+    /**
+     * Sets the look and feel of the application to the system look and feel.
+     */
     private static void setLookAndFeel() {
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (Exception e) {
-            logger.error("setLookAndFeel()", e);
+            System.err.println("Error in setLookAndFeel(): " + e);
         }
     }
 
+    /**
+     * Launches the main application frame.
+     */
     private static void launchApp() {
         SwingUtilities.invokeLater(MainFrame::new);
     }
